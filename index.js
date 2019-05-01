b9919915561aa6134f7737148d85e47ac91f798c
janat08 <jey.and.key@gmail.com>
2019-05-02 01:43:55 +0600

    all api covered, buggged

--- index.js
+++ index.js
@@ -1,36 +1,80 @@
-'use strict';
-const fs = require('fs');
-const arrayUnion = require('array-union');
-const glob = require('glob');
-const fastGlob = require('fast-glob');
-const dirGlob = require('dir-glob');
-const gitignore = require('./gitignore');
+"use strict";
+const fs = require("fs");
+const arrayUnion = require("array-union");
+const glob = require("glob");
+const fastGlob = require("fast-glob");
+const dirGlob = require("dir-glob");
+const gitignore = require("./gitignore");
 
-const isNegative = pattern => pattern[0] === '!';
+const isNegative = (pattern) => pattern[0] === "!";
 
-const assertPatternsInput = patterns => {
-	if (!patterns.every(x => typeof x === 'string')) {
-		throw new TypeError('Patterns must be a string or an array of strings');
+const assertPatternsInput = (patterns) => {
+	if (!patterns.every((x) => typeof x === "string")) {
+		throw new TypeError("Patterns must be a string or an array of strings");
 	}
 };
 
-const checkCwdOption = options => {
+const checkCwdOption = (options) => {
 	if (options && options.cwd && !fs.statSync(options.cwd).isDirectory()) {
-		throw new Error('The `cwd` option must be a path to a directory');
+		throw new Error("The `cwd` option must be a path to a directory");
 	}
 };
+
+function gitPatterns(cb, sync = true) {
+	return function(patterns, options) {
+		patterns = arrayUnion([].concat(patterns));
+		if (sync) {
+			assertPatternsInput(patterns);
+			checkCwdOption(options);
+		} else {
+			try {
+				assertPatternsInput(patterns);
+				checkCwdOption(options);
+			} catch (error) {
+				return Promise.reject(error);
+			}
+		}
+
+		if (!options || (options && !options.gitignore)) {
+			return cb(patterns, options);
+		}
+
+		const gitignoreStrings = () => {
+			return Promise.resolve(
+				patterns.concat(
+					gitignore.getPatterns({
+						cwd: options.cwd,
+						ignore: options.ignore
+					})
+				)
+			);
+		};
+
+		if (!sync) {
+			return gitignoreStrings().then(cb);
+		}
+
+		patterns = patterns.concat(
+			gitignore.getPatterns.sync({
+				cwd: options.cwd,
+				ignore: options.ignore
+			})
+		);
+
+		return cb(patterns, options);
+	};
+}
 
 const generateGlobTasks = (patterns, taskOptions) => {
-	patterns = arrayUnion([].concat(patterns));
-	assertPatternsInput(patterns);
-	checkCwdOption(taskOptions);
-
 	const globTasks = [];
 
-	taskOptions = Object.assign({
-		ignore: [],
-		expandDirectories: true
-	}, taskOptions);
+	taskOptions = Object.assign(
+		{
+			ignore: [],
+			expandDirectories: true
+		},
+		taskOptions
+	);
 
 	patterns.forEach((pattern, i) => {
 		if (isNegative(pattern)) {
@@ -40,13 +84,13 @@ const generateGlobTasks = (patterns, taskOptions) => {
 		const ignore = patterns
 			.slice(i)
 			.filter(isNegative)
-			.map(pattern => pattern.slice(1));
+			.map((pattern) => pattern.slice(1));
 
 		const options = Object.assign({}, taskOptions, {
 			ignore: taskOptions.ignore.concat(ignore)
 		});
 
-		globTasks.push({pattern, options});
+		globTasks.push({ pattern, options });
 	});
 
 	return globTasks;
@@ -59,19 +103,26 @@ const globDirs = (task, fn) => {
 	}
 
 	if (Array.isArray(task.options.expandDirectories)) {
-		options = Object.assign(options, {files: task.options.expandDirectories});
-	} else if (typeof task.options.expandDirectories === 'object') {
+		options = Object.assign(options, {
+			files: task.options.expandDirectories
+		});
+	} else if (typeof task.options.expandDirectories === "object") {
 		options = Object.assign(options, task.options.expandDirectories);
 	}
 
 	return fn(task.pattern, options);
 };
 
-const getPattern = (task, fn) => task.options.expandDirectories ? globDirs(task, fn) : [task.pattern];
+const getPattern = (task, fn) =>
+	task.options.expandDirectories ? globDirs(task, fn) : [task.pattern];
 
-const globToTask = task => glob => {
-	const {options} = task;
-	if (options.ignore && Array.isArray(options.ignore) && options.expandDirectories) {
+const globToTask = (task) => (glob) => {
+	const { options } = task;
+	if (
+		options.ignore &&
+		Array.isArray(options.ignore) &&
+		options.expandDirectories
+	) {
 		options.ignore = dirGlob.sync(options.ignore);
 	}
 
@@ -83,73 +134,59 @@ const globToTask = task => glob => {
 
 const globby = (patterns, options) => {
 	let globTasks;
-	patterns = arrayUnion([].concat(patterns));
 
 	try {
-		assertPatternsInput(patterns);
-		checkCwdOption(options);
+		globTasks = generateGlobTasks(patterns, options);
 	} catch (error) {
 		return Promise.reject(error);
 	}
 
-	const gitignoreStrings = () => {
-		return Promise.resolve(
-			options && options.gitignore ?
-				gitignore.getPatterns({cwd: options.cwd, ignore: options.ignore}) :
-				[]
-		);
-	};
+	const getTasks = Promise.all(
+		globTasks.map((task) =>
+			Promise.resolve(getPattern(task, dirGlob)).then((globs) =>
+				Promise.all(globs.map(globToTask(task)))
+			)
+		)
+	).then((tasks) => arrayUnion(...tasks));
 
-	return gitignoreStrings().then(gitignoreStrings => {
-		console.log();
-
-		try {
-			globTasks = generateGlobTasks(patterns.concat(gitignoreStrings), options);
-		} catch (error) {
-			return Promise.reject(error);
-		}
-
-		const getTasks = Promise.all(globTasks.map(task => Promise.resolve(getPattern(task, dirGlob))
-			.then(globs => Promise.all(globs.map(globToTask(task))))
-		))
-			.then(tasks => arrayUnion(...tasks));
-
-		return getTasks
-			.then(tasks => Promise.all(tasks.map(task => fastGlob(task.pattern, task.options))))
-			.then(paths => arrayUnion(...paths))
-			.then(res => {
-				return res;
-			});
-	});
+	return getTasks
+		.then((tasks) =>
+			Promise.all(
+				tasks.map((task) => fastGlob(task.pattern, task.options))
+			)
+		)
+		.then((paths) => arrayUnion(...paths))
+		.then((res) => {
+			return res;
+		});
 };
 
-module.exports = globby;
+module.exports = gitPatterns(globby, false);
 // TODO: Remove this for the next major release
-module.exports.default = globby;
+module.exports.default = gitPatterns(globby, false);
 
-module.exports.sync = (patterns, options) => {
+module.exports.sync = gitPatterns((patterns, options) => {
 	patterns = arrayUnion([].concat(patterns));
 	assertPatternsInput(patterns);
 	checkCwdOption(options);
-	const gitignoreStrings = options && options.gitignore ?
-		gitignore.getPatterns.sync({cwd: options.cwd, ignore: options.ignore}) :
-		[];
-	const globTasks = generateGlobTasks(patterns.concat(gitignoreStrings), options);
+
+	const globTasks = generateGlobTasks(patterns, options);
 	const tasks = globTasks.reduce((tasks, task) => {
 		const newTask = getPattern(task, dirGlob.sync).map(globToTask(task));
 		return tasks.concat(newTask);
 	}, []);
 
 	return tasks.reduce(
-		(matches, task) => arrayUnion(matches, fastGlob.sync(task.pattern, task.options)),
+		(matches, task) =>
+			arrayUnion(matches, fastGlob.sync(task.pattern, task.options)),
 		[]
 	);
-};
+});
 
-module.exports.generateGlobTasks = generateGlobTasks;
+module.exports.generateGlobTasks = gitPatterns(generateGlobTasks);
 
-module.exports.hasMagic = (patterns, options) => []
-	.concat(patterns)
-	.some(pattern => glob.hasMagic(pattern, options));
+module.exports.hasMagic = gitPatterns((patterns, options) =>
+	[].concat(patterns).some((pattern) => glob.hasMagic(pattern, options))
+);
 
 module.exports.gitignore = gitignore;
